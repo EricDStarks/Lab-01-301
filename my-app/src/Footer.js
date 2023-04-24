d9e3f6d888a1c541527e6da9619f2f33ce1dd3ee
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-04-24 11:31:23 -0500

    app

---
+++ my-app/src/Footer.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+function Footer(props) {
+  let myName = "Eric Starks"
+  return (
+    <p>{myName}</p>
+  );
+}
+
+export default Footer;
---
+++ my-app/src/Header.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header(props) {
+  let myName = "Eric Starks"
+  return (
+    <h1>Lab1</h1>
+  );
+}
+
+export default Header;
