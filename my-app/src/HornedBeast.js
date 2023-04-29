b386fcd6ed91da07c6ecd13b13f58597e63b438c
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-04-29 15:47:22 -0500

    javascript

--- my-app/src/HornedBeast.js
+++ my-app/src/HornedBeast.js
@@ -5,18 +5,27 @@ import Footer from './Footer';
 import {useState} from'react'
 import Card from 'react-bootstrap/Card';
 function HornedBeast(props) {
-  //set likes with event listener by creating state
   const [likes, setLikes] =useState(0)
+  const [heartLikes, setHeartLikes] =useState(0)
+  let heart = "💙"
+  
   function onClick(){
 
    setLikes(likes+1)
   }
+
+  function heartClick(){
+
+    setHeartLikes(heartLikes+1)
+  }
+
   return (
     <Card>
     <h2>{props.title}</h2>
     <img onClick={onClick} src={props.image} width="200" height="200" alt="HornedBeast" title="HornedBeast"></img>
     <p>{props.description}</p>
-    💙: {likes}
+    
+    <button onClick={heartClick}></button>
     </Card>
   );
 }
