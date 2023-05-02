9ec9c9200be7188048bac351942da5bd2af64e94
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-05-02 10:19:48 -0500

    Update SearchBeast.js

---
+++ my-app/src/SearchBeast.js
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import Form from 'react-bootstrap';
+import Button from 'react-bootstrap';
+
+function FilterBeast({}) {
+    const [selectedChoice, setSelectedChoice] = useState();
+
+    
+}
