906620a130b8bcdd16b6bb8e9fcf0f2487134f36
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-05-03 04:08:55 -0500

    js

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -4,7 +4,7 @@ import './App.css';
 function Header(props) {
   let myName = "Eric Starks"
   return (
-    <h1>Lab1</h1>
+    <h1>Horned <img src="https://orig00.deviantart.net/7824/f/2008/274/8/2/horned_beast_by_dikdiz.jpg" width="100"></img> Beasts</h1>
   );
 }
 
--- my-app/src/SearchBeast.js
+++ my-app/src/SearchBeast.js
@@ -2,21 +2,38 @@ import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HornedBeast from './HornedBeast';
+import BeastData from './data.json';
 
 
-function SearchBeast() {
-    const [choice, setChoice] = useState("One")
+function SearchBeast(props) {
+    const [choice, setChoice] = useState("1")
+    let BeastData = [];
+    var beasts = [<HornedBeast />, <HornedBeast />]
+    var filteredData = BeastData.filter(function(element){
+        if (element.horns == choice) {
+            return true
+        }
+        else {
+            return false
+        }
+    })
     return (
         <div className="App">
             <Form.Select onChange={function(event){
                 //choice = event.target.value
                 setChoice(event.target.value)
             }} aria-lable="Default select example">
-                <option>Open this menu</option>
-                <option value="One">One</option>
-                <option value="One">One</option>
-                <option value="One">One</option>
+                <option>Select Horns Amount</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="100">Hundred</option>
             </Form.Select>
+        {filteredData.map ((element) => {
+                return <HornedBeast setSelectedBeast={props.setSelectedBeast} updateFunction={props.updateFunction} imgurl={element.img_url} description={element.description} title={element.title} />  
+            })}
+        <h1>{props.title}</h1>
+        <h1>{choice}</h1>    
         </div>
     )
 }
